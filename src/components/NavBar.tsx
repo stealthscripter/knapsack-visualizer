7c29085c3e3f1f13004d563b7798b3c59aa8a704
stealthscripter <natnael01ayele@gmail.com>
2025-03-09 15:50:39 +0300

    add gitx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
+import { Github } from "lucide-react"
+
 function NavBar() {
   return (
     <div className='col-start-2 col-end-7 font-josefin py-8 px-4 text-zinc-200'>
         <ul>
             <li className='md:text-xl font-bold'>
-                NatSquad
+            <a href="https://github.com/stealthscripter"><Github className="cursor-pointer hover:stroke-zinc-300 duration-300" /></a>
             </li>
         </ul>
     </div>
